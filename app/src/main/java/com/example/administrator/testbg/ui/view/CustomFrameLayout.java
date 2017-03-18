ef53c6737c39ed1f12b7a08d0bcd5e18f72e76c8
guliping <1058719078@qq.com>
2017-03-18 23:14:31 +0800

    去掉LogUtil

--- app/src/main/java/com/example/administrator/testbg/ui/view/CustomFrameLayout.java
+++ app/src/main/java/com/example/administrator/testbg/ui/view/CustomFrameLayout.java
@@ -5,7 +5,9 @@ import android.graphics.Canvas;
 import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.FrameLayout;
 
@@ -16,59 +18,65 @@ import simple.util.tools.LogUtil;
  */
 
 public class CustomFrameLayout extends FrameLayout {
-    static final String TAG = "CustomFrameLayout";
+	static final String TAG = "CustomFrameLayout";
 
-    public CustomFrameLayout(@NonNull Context context) {
-        this(context, null);
-    }
+	public CustomFrameLayout(@NonNull Context context) {
+		this(context, null);
+	}
 
-    public CustomFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
+	public CustomFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
 
-    public CustomFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+	public CustomFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        LogUtil.i(TAG, "onMeasure" + "," + getContentDescription());
-    }
+		LogUtil.i(TAG, "onMeasure" + "," + getContentDescription());
+	}
 
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
+	@Override
+	protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+		super.onLayout(changed, left, top, right, bottom);
 
-        LogUtil.i(TAG, "onLayout" + "," + getContentDescription());
-    }
+		LogUtil.i(TAG, "onLayout" + "," + getContentDescription());
+	}
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
 
-        LogUtil.i(TAG, "onDraw" + "," + getContentDescription());
-    }
+		LogUtil.i(TAG, "onDraw" + "," + getContentDescription());
+	}
 
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        LogUtil.i(TAG, "dispatchTouchEvent" + "," + getContentDescription());
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev) {
+		Log.i(TAG, "dispatchTouchEvent" + "," + getContentDescription() + ",action=" + ev.getAction());
 
-        return super.dispatchTouchEvent(ev);
-    }
+//		if (TextUtils.equals(getContentDescription(), "2"))
+//			return true;
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        LogUtil.i(TAG, "onTouchEvent" + "," + getContentDescription());
+		return super.dispatchTouchEvent(ev);
+	}
 
-        return super.onTouchEvent(event);
-    }
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		Log.i(TAG, "onTouchEvent" + "," + getContentDescription() + ",action=" + event.getAction());
 
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        LogUtil.i(TAG, "onInterceptTouchEvent" + "," + getContentDescription());
+		return super.onTouchEvent(event);
+	}
 
-        return super.onInterceptTouchEvent(ev);
-    }
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		Log.i(TAG, "onInterceptTouchEvent" + "," + getContentDescription() + ",action=" + ev.getAction());
+
+//		if (TextUtils.equals(getContentDescription(), "2"))
+//			return true;
+
+		return super.onInterceptTouchEvent(ev);
+	}
 }
--- app/src/main/java/com/example/administrator/testbg/ui/view/CustomView.java
+++ app/src/main/java/com/example/administrator/testbg/ui/view/CustomView.java
@@ -3,7 +3,9 @@ package com.example.administrator.testbg.ui.view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -14,52 +16,55 @@ import simple.util.tools.LogUtil;
  */
 
 public class CustomView extends View {
-    static final String TAG = "CustomView";
+	static final String TAG = "CustomView";
 
-    public CustomView(Context context) {
-        this(context, null);
-    }
+	public CustomView(Context context) {
+		this(context, null);
+	}
 
-    public CustomView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
+	public CustomView(Context context, @Nullable AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
 
-    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+	public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        LogUtil.i(TAG, "onMeasure" + "," + getContentDescription());
-    }
+		LogUtil.i(TAG, "onMeasure" + "," + getContentDescription());
+	}
 
-    @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
+	@Override
+	protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+		super.onLayout(changed, left, top, right, bottom);
 
-        LogUtil.i(TAG, "onLayout" + "," + getContentDescription());
-    }
+		LogUtil.i(TAG, "onLayout" + "," + getContentDescription());
+	}
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
 
-        LogUtil.i(TAG, "onDraw" + "," + getContentDescription());
-    }
+		LogUtil.i(TAG, "onDraw" + "," + getContentDescription());
+	}
 
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        LogUtil.i(TAG, "dispatchTouchEvent" + "," + getContentDescription());
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev) {
+		Log.i(TAG, "dispatchTouchEvent" + "," + getContentDescription() + ",action=" + ev.getAction());
 
-        return super.dispatchTouchEvent(ev);
-    }
+		return super.dispatchTouchEvent(ev);
+	}
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        LogUtil.i(TAG, "onTouchEvent" + "," + getContentDescription());
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		Log.i(TAG, "onTouchEvent" + "," + getContentDescription() + ",action=" + event.getAction());
 
-        return super.onTouchEvent(event);
-    }
+//		if (TextUtils.equals(getContentDescription(), "4"))
+//			return true;
+
+		return super.onTouchEvent(event);
+	}
 }
